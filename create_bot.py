0f26ee22d6f3c0100c0674f47c00fcbbec9a89c5
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-12-11 16:51:11 +0200

    channel_select

--- create_bot.py
+++ create_bot.py
@@ -1,3 +1,5 @@
+# Developed by nomiss7
+
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -11,9 +13,8 @@ storage = MemoryStorage()
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot, storage=storage)
 
-# chat_id = -1001844933868
-chat_id = -1001834053104
+chat_id = -1001685066059
 
 # mute time
-# time_example = 21600
-time_example = 60
+time_example = 21600
+# time_example = 60
